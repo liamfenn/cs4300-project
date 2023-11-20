7982ff02fad63fdae33b511b7577959b56116db3
liamfenn <lgf63569@uga.edu>
2023-11-20 17:20:29 -0500

    Added Login Responsiveness

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import ReservationItem from './components/ReservationItem';
 import FilterBtn from './components/FilterBtn';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    setShowLogin(false);
+  };
+
+  const toggleLogin = () => {
+    setShowLogin(!showLogin);
+  };
+
   return (
     <body>
-      <Navbar/>
+      <Navbar isLoggedIn={isLoggedIn} onLoginClick={toggleLogin} />
+      {showLogin && <Login onLogin={handleLogin} />}
       <FilterBtn/>
       <ReservationItem/>
     </body>
